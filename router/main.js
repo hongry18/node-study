db3939da7695819e0270109d48051d550e2c1259
hongry18 <hongry18@gmail.com>
2017-03-07 10:38:59 +0000

    restful sample

--- router/main.js
+++ router/main.js
@@ -1,18 +1,31 @@
-module.exports = function(app)
-{
-    app.get(
-        '/',
-        function(req, res)
-        {
-            res.render('index.html');
-        }
-    );
+module.exports = function( app, fs ) {
 
-    app.get(
-        '/about',
-        function(req, res)
-        {
-            res.render('about.html');
-        }
-    );
+    app.get( '/', function( req, res ) {
+        res.render( 'index.html' );
+    });
+
+    app.get( '/about', function( req, res ) {
+        res.render( 'about.html' );
+    });
+
+    app.get( '/ejs', function( req, res ) {
+        res.render( 'index', {
+            title: 'My Honmpage',
+            length: 5
+        })
+    });
+
+    app.get( '/rest1', function( req, res ) {
+        fs.readFile( __dirname + '/../data/' + 'data.json', 'utf-8', function( err, data ) {
+            res.end( data );
+        });
+    });
+
+    app.get( '/rest2/:arg', function( req, res ) {
+        fs.readFile( __dirname + '/../data/' + 'data.json', 'utf-8', function( err, data ) {
+            var _data = JSON.parse( data );
+            res.json( _data[req.params.arg] );
+        });
+    });
+
 }
--- server.js
+++ server.js
@@ -1,27 +1,38 @@
-// load express module
+// load modules
 var express = require( 'express' );
-
-// init express
 var app = express();
-
-// load module
-var router = require( './router/main.js' )(app);
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var fs = require("fs")
 
 // set html path
 app.set( 'views', __dirname + '/views' );
 
-// using ejs engine when html rendering to server 
+// using ejs engine when server renders html
 app.set( 'view engine', 'ejs' );
 app.engine( 'html', require('ejs').renderFile );
 
 // listen port 3000
 var server = app.listen (
     3000,
-    function()
-    {
+    function() {
         console.log( 'express start http://192.168.99.100:1002' );
     }
 );
 
 // using static file in public directory
 app.use( express.static('public') );
+
+// using ejs
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded() );
+app.use(
+    session({
+        secret: '#@test@#',
+        resave: false,
+        saveUninitialized: true
+    })
+);
+
+// load custom router
+var router = require( './router/main.js' )( app, fs );

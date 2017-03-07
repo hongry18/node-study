883a56827106aa81f1ec95743e8965140fb12ff2
hongry18 <hongry18@gmail.com>
2017-03-07 08:26:01 +0000

    add static file directory

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+// load express module
 var express = require( 'express' );
+
+// init express
 var app = express();
+
+// load module
 var router = require( './router/main.js' )(app);
 
+// set html path
 app.set( 'views', __dirname + '/views' );
+
+// using ejs engine when html rendering to server 
 app.set( 'view engine', 'ejs' );
 app.engine( 'html', require('ejs').renderFile );
 
+// listen port 3000
 var server = app.listen (
     3000,
     function()
@@ -13,3 +22,6 @@ var server = app.listen (
         console.log( 'express start http://192.168.99.100:1002' );
     }
 );
+
+// using static file in public directory
+app.use( express.static('public') );
